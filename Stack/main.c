cc8608fb6cbdf893dfa046d19e429bc69a724541
ajlekcahdp4 <89397750+ajlekcahdp4@users.noreply.github.com>
2021-11-09 16:28:21 +0300

    Update main.c

--- Stack/main.c
+++ Stack/main.c
@@ -1,24 +1,28 @@
-#include "Stack_test.h"
+#include "Stack.h"
 int main (void)
 {
     Stack stk = {};
-    StackCtor(&stk, 10);
-    $meow
-    StackPush(&stk, 1);
-    $meow
-    StackPush(&stk, 2);
-    $meow
-    StackPush(&stk, 2);
-    $meow
-    StackPush(&stk, 2);
-    StackPush(&stk, 2);
-    StackPush(&stk, 2);
-    double x;
+    StackCtor (&stk, 10);
+    StackPush (&stk, 100);
+    StackPush (&stk, 100);
+    StackPush (&stk, 100);
+    StackPush (&stk, 100);
+    StackPush (&stk, 100);
+    StackPush (&stk, 100);
+    StackPush (&stk, 100);
+    StackPush (&stk, 100);
+    StackPush (&stk, 100);
+    StackPush (&stk, 100);
+    StackPush (&stk, 100);
+    StackPush (&stk, 100);
+    double x = 0;
     StackPop(&stk, &x);
-    printf("%f\n", x);
     StackPop(&stk, &x);
-    printf("%f\n", x);
-
+    StackPop(&stk, &x);
+    printf("%g\n", x);
+    StackPop(&stk, &x);
+    printf("%g\n", x);
     StackDtor(&stk);
     return 0;
 }
+
