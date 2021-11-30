dd3c807e8f329a08bc8a32d6c9cdae9c46305b1f
Alex Romanov <89397750+ajlekcahdp4@users.noreply.github.com>
2021-11-30 21:36:33 +0300

    Update main.c

--- Processor/main.c
+++ Processor/main.c
@@ -1,13 +1,11 @@
-
 #include "Reading.h"
 #include "assembler.h"
+#include "Processor.h"
 int main()
 {
-    char * Buf = 0;
-    int ch_numb = FillBuffer (&Buf);
-    int s_numb = StrNumber(Buf, ch_numb);
-    char** Index = (char**)calloc (s_numb, sizeof (char*));
-    FillIndex (Index, s_numb, ch_numb, Buf);
-    int * CODE = CodeGenerate (Index, s_numb);
+    char * buffer = 0;
+    int ch_numb = FillBuffer (&buffer);
+    CodeGenerate (buffer, ch_numb);//Сассемблировали, записали в файл и закрыли его
+    RunCode();
     return 0;
 }
