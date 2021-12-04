22cf86970325a3e1f7d44df19f580bf3c63da866
Alex Romanov <89397750+ajlekcahdp4@users.noreply.github.com>
2021-12-04 13:18:41 +0300

    Update main.c

--- Processor/main.c
+++ Processor/main.c
@@ -1,11 +1,17 @@
+#include <string.h>
 #include "Reading.h"
 #include "assembler.h"
 #include "Processor.h"
+#include "Stack.h"
+
+FILE* log_file = 0;
+
 int main()
 {
     char * buffer = 0;
-    int ch_numb = FillBuffer (&buffer);
-    CodeGenerate (buffer, ch_numb);//Сассемблировали, записали в файл и закрыли его
-    RunCode();
+    int ch_numb = FillBuffer (&buffer, log_file);
+    CodeGenerate (buffer, ch_numb, log_file);//Сассемблировали, записали в файл и закрыли его
+    Processor(log_file);
     return 0;
 }
+
