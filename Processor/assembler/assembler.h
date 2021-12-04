c1db72fd39bb772c6874f6f4cf25d1ed63196466
Alex Romanov <89397750+ajlekcahdp4@users.noreply.github.com>
2021-12-04 13:18:11 +0300

    Update assembler.h

--- Processor/assembler/assembler.h
+++ Processor/assembler/assembler.h
@@ -20,9 +20,9 @@ enum CMDS
 };
 
 
-void CodeGenerate (char* buffer, int ch_numb);
+void CodeGenerate (char* buffer, int ch_numb, FILE* log_file);
 int StrnCompare(const char *str1, const char *str2, int n);
-int FindArg (char * comand_line);
-void CmdCode (int * code, int*caret, int cmd, char * comand_line);
+int FindArg (char * comand_line, FILE* log_file);
+void CmdCode (int * code, int*caret, int cmd, char * comand_line, FILE* log_file);
 int CmdNumber (char * comand_line);
 #endif
