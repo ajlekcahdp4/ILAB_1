4077989b81c1909961d7aa7e2745ae5aa70bf27b
Alex Romanov <89397750+ajlekcahdp4@users.noreply.github.com>
2021-12-06 12:51:29 +0300

    defined

--- Processor/assembler/assembler.c
+++ Processor/assembler/assembler.c
@@ -1,4 +1,17 @@
 #include "assembler.h"
+#define DEF_CMD(name, num, args, ...)                                           \
+    if (StrnCompare(comand_line, #name, strlen(#name)- 1) == 0)                 \
+    {                                                                           \
+            code[*ip] = num;                                                    \
+            *ip += 1;                                                           \
+            if (args)                                                           \
+            {                                                                   \
+                code[*ip] = FindArg(comand_line + strlen(#name), log_file);     \
+                *ip += 1;                                                       \
+            }                                                                   \
+    }                                                                           \
+    else
+
 
 int StrnCompare(const char *str1, const char *str2, int n)
 {
@@ -19,28 +32,28 @@ void CodeGenerate (char * buffer, int ch_numb, FILE* log_file)
     assert (buffer);
     FILE * code_file = fopen ("code.bin", "wb");
     int cmd = 0;
-    int caret = 0;
+    int ip = 0;
     int* code = (int*) calloc (ch_numb, sizeof(int));
     assert (code);
 
-
-    cmd = CmdNumber(buffer);
-    CmdCode (code, &caret, cmd, buffer, log_file);
+    CmdCode (code, &ip, buffer, log_file);
+    //printf("\n%5s\n", buffer);
     for (int  i = 0; i < ch_numb; i++)
     {
         if (buffer[i] == '\n' || buffer[i] == EOF)
         {
-            cmd = CmdNumber(buffer + i + 1);
-            CmdCode (code, &caret, cmd, buffer + i + 1, log_file);
+            //printf("\n%5s\n", buffer+i+1);
+            CmdCode (code, &ip, buffer + i + 1, log_file);
         }
     }
 
-    /*
-    for (int i = 0; i < caret; i ++)
-        printf("%d\n", code[i]);
-    */
+    //printf ("ip = %d\n", ip);
 
-    fwrite (code, sizeof(int), caret, code_file);
+    //for (int i = 0; i < ip; i ++)
+      //  printf("<%d>\n", code[i]);
+
+
+    fwrite (code, sizeof(int), ip, code_file);
     free(code);
     fclose(code_file);
 }
@@ -54,7 +67,7 @@ int FindArg (char * comand_line, FILE* log_file)
     int ARG = 0;
     int i = 0;
     int len = strlen (comand_line);
-    while (comand_line[i] == ' ' || comand_line[i] == '\t')
+    while ((comand_line[i] == ' ' || comand_line[i] == '\t') && comand_line[i] != '\n')
             i++;
     while (comand_line[i] != '\n')
     {
@@ -65,8 +78,7 @@ int FindArg (char * comand_line, FILE* log_file)
         }
         else
         {
-            fprintf(log_file, "ERROR: Wrong argument\n");
-            fprintf(log_file, "ERROR: Found argument (%d)\n", ARG);
+            fprintf(log_file, "ERROR: Found argument (%c)\n", comand_line[i]);
         }
         i++;
     }
@@ -74,79 +86,13 @@ int FindArg (char * comand_line, FILE* log_file)
 }
 
 
-
-
-void CmdCode (int * code, int*caret, int cmd, char * comand_line, FILE* log_file)
+void CmdCode (int * code, int*ip, char * comand_line, FILE* log_file)
 {
     assert(code);
     assert(comand_line);
+    assert (log_file);
     int ARG = 0;
-    switch (cmd)
-    {
-    case PUSH:
-        ARG = FindArg(comand_line + 4, log_file);
-        code[*caret] = cmd;
-        *caret += 1;
-        code[*caret] = ARG;
-        *caret += 1;
-        break;
-    case MUL:
-        code[*caret] = cmd;
-        *caret += 1;
-        break;
-    case SUB:
-        code[*caret] = cmd;
-        *caret += 1;
-        break;
-    case OUT:
-        code[*caret] = cmd;
-        *caret += 1;
-        break;
-    case ADD:
-        code[*caret] = cmd;
-        *caret += 1;
-        break;
-    case DIV:
-        code[*caret] = cmd;
-        *caret += 1;
-        break;
-    case SQRT:
-        code[*caret] = cmd;
-        *caret += 1;
-        break;
-    case IN:
-        code[*caret] = cmd;
-        *caret += 1;
-        break;
-    case HLT:
-        code[*caret] = cmd;
-        *caret += 1;
-        break;
-    default:
-        fprintf(log_file, "\nERROR: Wrong command number (%d)\n", cmd);
-        break;
-    }
+    #include "commands.h"
+    fprintf(log_file, "ERROR: NFC in line.\n");
 }
-
-int CmdNumber (char * comand_line)
-{
-    if (StrnCompare (comand_line, "push", 3) == 0)
-        return PUSH;
-    else if (StrnCompare (comand_line, "mul", 2) == 0)
-        return MUL;
-    else if (StrnCompare (comand_line, "sub", 2) == 0)
-        return SUB;
-    else if (StrnCompare (comand_line, "out", 2) == 0)
-        return OUT;
-    else if (StrnCompare (comand_line, "add", 2) == 0)
-        return ADD;
-    else if (StrnCompare (comand_line, "div", 2) == 0)
-        return DIV;
-    else if (StrnCompare (comand_line, "sqrt", 3) == 0)
-        return SQRT;
-    else if (StrnCompare (comand_line, "hlt", 2) == 0)
-        return HLT;
-    else if (StrnCompare (comand_line, "in", 1) == 0)
-        return IN;
-    return SINTAXIS_EROR;
-}
+#undef DEF_CMD
