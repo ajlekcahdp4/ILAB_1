6521a7b0126b21deccb4811df98452a8222e8e3c
Alex Romanov <89397750+ajlekcahdp4@users.noreply.github.com>
2021-12-06 13:07:09 +0300

    Update assembler.h

--- Processor/assembler/assembler.h
+++ Processor/assembler/assembler.h
@@ -7,22 +7,21 @@
 #define $meow printf("meow from %s (%d) from %s\n", __FILE__, __LINE__, __PRETTY_FUNCTION__);
 enum CMDS
 {
-    HLT = -1,
-    SINTAXIS_EROR = 0,
-    PUSH = 1,
-    MUL = 2,
-    SUB = 4,
-    OUT,
-    ADD,
-    DIV,
-    SQRT,
-    IN
+    hlt = -1,
+    sintaxis_error = 0,
+    push = 1,
+    add = 2,
+    sub = 3,
+    mul = 4,
+    out = 5,
+    divv,
+    in
 };
 
 
 void CodeGenerate (char* buffer, int ch_numb, FILE* log_file);
 int StrnCompare(const char *str1, const char *str2, int n);
 int FindArg (char * comand_line, FILE* log_file);
-void CmdCode (int * code, int*caret, int cmd, char * comand_line, FILE* log_file);
+void CmdCode (int * code, int*ip, char * comand_line, FILE* log_file);
 int CmdNumber (char * comand_line);
 #endif
