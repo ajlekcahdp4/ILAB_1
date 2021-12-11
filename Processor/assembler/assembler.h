650ff4229b421e04ebca36b3529093c435330c6f
Alex Romanov <89397750+ajlekcahdp4@users.noreply.github.com>
2021-12-11 19:29:55 +0300

    support registers

--- Processor/assembler/assembler.h
+++ Processor/assembler/assembler.h
@@ -7,6 +7,8 @@
 void Assembler (char* buffer, int ch_numb, FILE* log_file);
 int StrnCompare(const char *str1, const char *str2, int n);
 int FindArg (char * comand_line, FILE* log_file);
-void CmdCode (int * code, int*ip, char * comand_line, FILE* log_file);
+int FindReg (char * comand_line, FILE* log_file);
+void SkipSpaces (char* comand_line, int *i, FILE * log_file);
+void CmdCode (char * code, int*ip, char * comand_line, FILE* log_file);
 int CmdNumber (char * comand_line);
 #endif
