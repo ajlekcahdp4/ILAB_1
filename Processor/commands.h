cd55b5c96db94ee51987f98867b7b29fbea5b8a8
Alex Romanov <89397750+ajlekcahdp4@users.noreply.github.com>
2021-12-11 19:22:42 +0300

    with registers

--- Processor/commands.h
+++ Processor/commands.h
@@ -1,48 +1,64 @@
 DEF_CMD(push, 1, 1, {
-    StackPush (&stk, code[*ip+1]);
-    *ip += 1;
+    if (code[*ip + 1] == 0)
+    {
+        StackPush (Data->stk, code[*ip+2]);
+        *ip += 5;
+    }
+    else
+    {
+        StackPush (Data->stk, Data->reg[(int)(code[*ip + 2])]);
+        *ip += 2;
+    }
+    StackDump (Data->stk, "push");
 })
 
 DEF_CMD(add, 2, 0, {
-    StackPop (&stk, &x);
-    StackPop (&stk, &y);
-    StackPush (&stk, x + y);
+    StackPop (Data->stk, &x);
+    StackPop (Data->stk, &y);
+    StackPush (Data->stk, x + y);
 })
 
 DEF_CMD(sub, 3, 0, {
-    StackPop (&stk, &x);
-    StackPop (&stk, &y);
-    StackPush (&stk, y - x);
+    StackPop (Data->stk, &x);
+    StackPop (Data->stk, &y);
+    StackPush (Data->stk, y - x);
 })
 
 
 DEF_CMD(out, 4, 0, {
-    StackPop(&stk, &x);
+    StackPop(Data->stk, &x);
     printf("%d\n", x);
 })
 
 DEF_CMD(mul, 5, 0, {
-    StackPop(&stk, &x);
-    StackPop(&stk, &y);
-    StackPush(&stk, x*y);
+    StackPop(Data->stk, &x);
+    StackPop(Data->stk, &y);
+    StackPush(Data->stk, x*y);
 })
 
 DEF_CMD(DIV, 6, 0, {
-    StackPop(&stk, &x);
-    StackPop(&stk, &y);
+    StackPop(Data->stk, &x);
+    StackPop(Data->stk, &y);
     if (x != 0)
-        StackPush(&stk, y/x);
+        StackPush(Data->stk, y/x);
     else
-        fprintf(log_file, "ERROR: devide by zero\n");
+        fprintf(log_file, "ERROR: devision by zero\n");
 })
 
 DEF_CMD (in, 7, 0, {
     scanf ("%d", &x);
-    StackPush (&stk, x);
+    StackPush (Data->stk, x);
+})
+
+DEF_CMD (pop, 8, 1, {
+    StackPop(Data->stk, &x);
+    Data->reg[(int)(code[*ip + 2])] = x;
+    *ip += 2;
+    StackDump (Data->stk, "pop");
 })
 
 DEF_CMD(hlt, -1, 0, {
-    StackDtor(&stk);
+    StackDtor(Data->stk);
     free(code);
     fclose(log_file);
 })
