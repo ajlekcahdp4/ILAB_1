bc1fadedde7f90febce50dbd45825225b08f12e6
Alex Romanov <89397750+ajlekcahdp4@users.noreply.github.com>
2021-12-14 13:47:31 +0300

    jmp command added

--- Processor/assembler/assembler.h
+++ Processor/assembler/assembler.h
@@ -3,12 +3,22 @@
 
 #include "includes.h"
 
+struct LABLES {
+    int b_numb;
+    char * lable_name;
+};
+typedef struct LABLES LABLES;
 
-void Assembler (char* buffer, int ch_numb, FILE* log_file);
-int StrnCompare(const char *str1, const char *str2, int n);
-int FindArg (char * comand_line, FILE* log_file);
-int FindReg (char * comand_line, FILE* log_file);
-void SkipSpaces (char* comand_line, int *i, FILE * log_file);
-void CmdCode (char * code, int*ip, char * comand_line, FILE* log_file);
-int CmdNumber (char * comand_line);
+
+int     FillBuffer (char ** buffer, FILE** log_file);
+void    Assembler (char* buffer, int ch_numb, FILE* log_file);
+int     TranslateToCode (char* buffer, int ch_numb, char* code, LABLES** lables, int *labl_cnt, FILE* log_file);
+int     StrnCompare(const char *str1, const char *str2, int n);
+int     FindArg (char * comand_line, FILE* log_file);
+int     FindReg (char * comand_line, FILE* log_file);
+void    SkipSpaces (char* comand_line, int *i, FILE * log_file);
+int     IsLable (char* command_line, LABLES** lables, int labl_cnt);
+
+void    CmdCode (char * code, int*ip, char * comand_line, LABLES ** lables, int *labl_cnt, FILE* log_file, int run_numb);
+int     CmdNumber (char * comand_line);
 #endif
