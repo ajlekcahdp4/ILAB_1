b71cb2f6fe060048dcf535cd0c5eef9872c93ffd
Alex Romanov <89397750+ajlekcahdp4@users.noreply.github.com>
2021-12-14 16:52:16 +0300

    all jumps added

--- Processor/assembler/assembler.h
+++ Processor/assembler/assembler.h
@@ -18,7 +18,7 @@ int     FindArg (char * comand_line, FILE* log_file);
 int     FindReg (char * comand_line, FILE* log_file);
 void    SkipSpaces (char* comand_line, int *i, FILE * log_file);
 int     IsLable (char* command_line, LABLES** lables, int labl_cnt);
-
+int     IsJump (char* name);
 void    CmdCode (char * code, int*ip, char * comand_line, LABLES ** lables, int *labl_cnt, FILE* log_file, int run_numb);
 int     CmdNumber (char * comand_line);
 #endif
