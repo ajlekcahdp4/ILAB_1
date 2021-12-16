8581daa436b73b427f2315e9e47906ad18ea549a
Alex Romanov <89397750+ajlekcahdp4@users.noreply.github.com>
2021-12-16 10:58:30 +0300

    release

--- Stack/main.c
+++ Stack/main.c
@@ -1,28 +1,23 @@
 #include "Stack.h"
+
 int main (void)
 {
-    Stack stk = {};
-    StackCtor (&stk, 10);
-    StackPush (&stk, 100);
-    StackPush (&stk, 100);
-    StackPush (&stk, 100);
-    StackPush (&stk, 100);
-    StackPush (&stk, 100);
-    StackPush (&stk, 100);
-    StackPush (&stk, 100);
-    StackPush (&stk, 100);
-    StackPush (&stk, 100);
-    StackPush (&stk, 100);
-    StackPush (&stk, 100);
-    StackPush (&stk, 100);
-    double x = 0;
-    StackPop(&stk, &x);
-    StackPop(&stk, &x);
-    StackPop(&stk, &x);
-    printf("%g\n", x);
-    StackPop(&stk, &x);
-    printf("%g\n", x);
-    StackDtor(&stk);
+    Stack * stk = (Stack*)calloc(1, sizeof(Stack));
+    StackCtor (stk, 1024);
+    StackPush (stk, 100);
+    StackPush (stk, 100);
+    StackPush (stk, 100);
+    StackPush (stk, 100);
+    StackPush (stk, 100);
+    StackPush (stk, 100);
+    int x = 0;
+    StackPop(stk, &x);
+    printf("%d\n", x);
+    StackPop(stk, &x);
+    printf("%d\n", x);
+    StackPop(stk, &x);
+    printf("%d\n", x);
+    StackDtor(stk);
     return 0;
-}
 
+}
