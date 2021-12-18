8146c07f4746b4a8910794a0fc7109ffbe73e11a
Alex Romanov <89397750+ajlekcahdp4@users.noreply.github.com>
2021-12-18 11:57:09 +0300

    macro ERROR added

--- Processor/assembler/assembler.h
+++ Processor/assembler/assembler.h
@@ -10,15 +10,19 @@ struct LABLES {
 typedef struct LABLES LABLES;
 
 
-int     FillBuffer (char ** buffer, FILE** log_file);
-void    Assembler (char* buffer, int ch_numb, FILE* log_file);
+#define ERROR(str)                   \
+    fclose(log_file);                \
+    assert(!#str);                   \
+
+int     FillBuffer      (char ** buffer, FILE** log_file);
+void    Assembler       (char* buffer, int ch_numb, FILE* log_file);
 int     TranslateToCode (char* buffer, int ch_numb, char* code, LABLES** lables, int *labl_cnt, FILE* log_file);
-int     StrnCompare(const char *str1, const char *str2, int n);
-int     FindArg (char * comand_line, FILE* log_file);
-int     FindReg (char * comand_line, FILE* log_file);
-void    SkipSpaces (char* comand_line, int *i, FILE * log_file);
-int     IsLable (char* command_line, LABLES** lables, int labl_cnt);
-int     IsJump (char* name);
-void    CmdCode (char * code, int*ip, char * comand_line, LABLES ** lables, int *labl_cnt, FILE* log_file, int run_numb);
-int     CmdNumber (char * comand_line);
+int     StrnCompare     (const char *str1, const char *str2, int n);
+int     FindArg         (char * comand_line, FILE* log_file);
+int     FindReg         (char * comand_line, FILE* log_file);
+void    SkipSpaces      (char* comand_line, int *i, FILE * log_file);
+int     IsLable         (char* command_line, LABLES** lables, int labl_cnt);
+int     IsEndOfStr      (char *command_line, int *i, FILE *log_file);
+int     IsJump          (char* name);
+void    CmdCode         (char * code, int*ip, char * comand_line, LABLES ** lables, int *labl_cnt, FILE* log_file, int run_numb);
 #endif
