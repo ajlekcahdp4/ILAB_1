9537dab5ef4a46f99ab1d4f38d728690c7922b55
Alex Romanov <89397750+ajlekcahdp4@users.noreply.github.com>
2021-12-20 14:18:08 +0300

    Update assembler.h

--- Processor/assembler/assembler.h
+++ Processor/assembler/assembler.h
@@ -3,6 +3,7 @@
 
 #include "includes.h"
 
+#include "Stack.h"
 struct LABLES {
     int b_numb;
     char * lable_name;
@@ -16,7 +17,7 @@ typedef struct LABLES LABLES;
 
 int     FillBuffer      (char ** buffer, FILE** log_file);
 void    Assembler       (char* buffer, int ch_numb, FILE* log_file);
-int     TranslateToCode (char* buffer, int ch_numb, char* code, LABLES** lables, int *labl_cnt, FILE* log_file);
+int     TranslateToCode (char* buffer, int ch_numb, char* code, LABLES** lables, int *labl_cnt, Stack* Rets, FILE* log_file);
 int     StrnCompare     (const char *str1, const char *str2, int n);
 int     FindArg         (char * comand_line, FILE* log_file);
 int     FindReg         (char * comand_line, FILE* log_file);
@@ -24,5 +25,5 @@ void    SkipSpaces      (char* comand_line, int *i, FILE * log_file);
 int     IsLable         (char* command_line, LABLES** lables, int labl_cnt);
 int     IsEndOfStr      (char *command_line, int *i, FILE *log_file);
 int     IsJump          (char* name);
-void    CmdCode         (char * code, int*ip, char * comand_line, LABLES ** lables, int *labl_cnt, FILE* log_file, int run_numb);
+void    CmdCode         (char * code, int*ip, char * comand_line, LABLES ** lables, int *labl_cnt, Stack* Rets, FILE* log_file, int run_numb);
 #endif
