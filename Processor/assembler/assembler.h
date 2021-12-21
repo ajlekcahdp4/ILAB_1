ad780489d7a1b03eb87a2a4e182e19a1d77f715f
Alex Romanov <89397750+ajlekcahdp4@users.noreply.github.com>
2021-12-21 12:25:07 +0300

    Update assembler.h

--- Processor/assembler/assembler.h
+++ Processor/assembler/assembler.h
@@ -3,7 +3,6 @@
 
 #include "includes.h"
 
-#include "Stack.h"
 struct LABLES {
     int b_numb;
     char * lable_name;
@@ -11,19 +10,20 @@ struct LABLES {
 typedef struct LABLES LABLES;
 
 
-#define ERROR(str)                   \
-    fclose(log_file);                \
-    assert(!#str);                   \
+#define ERROR(str)                                  \
+    fclose(log_file);                               \
+    assert(!#str);                                  \
 
 int     FillBuffer      (char ** buffer, FILE** log_file);
 void    Assembler       (char* buffer, int ch_numb, FILE* log_file);
-long long     TranslateToCode (char* buffer, int ch_numb, char* code, LABLES** lables, int *labl_cnt, Stack* Rets, FILE* log_file);
+int     TranslateToCode (char* buffer, int ch_numb, char* code, LABLES** lables, int *labl_cnt, FILE* log_file);
 int     StrnCompare     (const char *str1, const char *str2, int n);
 int     FindArg         (char * comand_line, FILE* log_file);
 int     FindReg         (char * comand_line, FILE* log_file);
 void    SkipSpaces      (char* comand_line, int *i, FILE * log_file);
 int     IsLable         (char* command_line, LABLES** lables, int labl_cnt);
+void    LablesCheck     (LABLES *lables, int labl_cnt, FILE* log_file);
 int     IsEndOfStr      (char *command_line, int *i, FILE *log_file);
 int     IsJump          (char* name);
-void    CmdCode         (char * code, long long*ip, char * comand_line, LABLES ** lables, int *labl_cnt, Stack* Rets, FILE* log_file, int run_numb);
+void    CmdCode         (char * code, int *ip, char * comand_line, LABLES ** lables, int *labl_cnt, FILE* log_file, int run_numb);
 #endif
